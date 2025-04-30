d2ffcd15fb9521c6b16c657209184887a7b070f8
justynize <nwobajusticeekene12@gamil.com>
2025-04-30 16:59:59 +0100

    updated

---
+++ file.js
@@ -0,0 +1,37 @@
+ 
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const submitBtn = document.getElementById('submitBtn');
+
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
+
+    function validateForm(){
+        let valid = true;
+
+        // ...email vaildation...
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(emailInput.value)) {
+            emailError.textContent = 'Please enter a valid email address.';
+            valid = false;
+        } else {
+            emailError.textContent = '';
+        }
+        // ...password validation...
+        if (passwordInput.value.length < 4) {
+            passwordError.textContent = 'Password must be at least 4 characters long.';
+            valid = false;
+        } else {
+            passwordError.textContent = '';
+        }
+       submitBtn.disabled = !valid;
+    }
+
+    emailInput.addEventListener('input', validateForm);
+    passwordInput.addEventListener('input', validateForm);
+
+    document.getElementById('myForm').addEventListener('submit', function (e){
+        e.preventDefault();
+        alert("Form submitted successfully!");
+    });
